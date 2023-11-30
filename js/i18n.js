13e9f38c8eec4f791ed1e61d97a46cb0045f3bd7
fsanzl <fsanzl@users.noreply.github.com>
2023-11-30 09:24:28 +0000

    deploy: 5f7d2fd1d747c21df8d3047eb72818af3c7af7cc

---
+++ js/i18n.js
@@ -0,0 +1,45 @@
+const lngs = {
+  en: { nativeName: 'English' },
+  de: { nativeName: 'Deutsch' },
+  it: { nativeName: 'Italiano' }
+};
+
+const rerender = () => {
+  $('body').localize();
+}
+
+$(function () {
+  // use plugins and options as needed, for options, detail see
+  // https://www.i18next.com
+  i18next
+    .use(i18nextHttpBackend)
+    .use(i18nextBrowserLanguageDetector)
+    // init i18next
+    // for all options read: https://www.i18next.com/overview/configuration-options
+    .init({
+      debug: true,
+      fallbackLng: 'en',
+      load: 'languageOnly',
+      backend: {
+        loadPath: 'locales/{{lng}}/translation.json',
+      }
+    }, (err, t) => {
+      if (err) return console.error(err);
+      jqueryI18next.init(i18next, $, { useOptionsAttr: true });
+      Object.keys(lngs).map((lng) => {
+        const opt = new Option(lngs[lng].nativeName, lng);
+        if (lng === i18next.resolvedLanguage) {
+          opt.setAttribute("selected", "selected");
+        }
+        $('#languageSwitcher').append(opt);
+      });
+      $('#languageSwitcher').change((a, b, c) => {
+        const chosenLng = $(this).find("option:selected").attr('value');
+        i18next.changeLanguage(chosenLng, () => {
+          rerender();
+        });
+      });
+
+      rerender();
+    });
+});

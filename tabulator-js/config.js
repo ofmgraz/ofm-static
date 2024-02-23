848a4ad88f9154de7cefaa7a576f85718520a945
fsanzl <fsanzl@users.noreply.github.com>
2024-02-23 12:47:46 +0000

    deploy: c3b1e24d86c0262097d518ff3f8cb868706ef971

--- tabulator-js/config.js
+++ tabulator-js/config.js
@@ -2,7 +2,9 @@
 
 var config = {
     height: 800,
-    layout: "fitColumns",
+    //minWidth: 700,
+    // layout: "fitColumns",
     tooltips: true,
-    dataLoader: true
+    dataLoader: true,
+    responsiveLayout:"collapse",
 };

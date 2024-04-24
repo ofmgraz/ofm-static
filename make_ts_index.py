5fce4fd15a41245ae04b3c9193bb1e7b85c27c6f
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-04-24 10:16:05 +0200

    more facets

--- make_ts_index.py
+++ make_ts_index.py
@@ -31,7 +31,10 @@ current_schema = {
         {"name": "year", "type": "string", "facet": True, "optional": True},
         {"name": "form", "type": "string", "facet": True, "optional": True},
         {"name": "genre", "type": "string[]", "facet": True, "optional": True},
+        {"name": "liturgy", "type": "string", "facet": True, "optional": True},
+        {"name": "provenance", "type": "string[]", "facet": True, "optional": True},
         {"name": "persons", "type": "string[]", "facet": True, "optional": True},
+
     ],
 }
 
@@ -70,6 +73,37 @@ def get_entities(ent_type, ent_node, ent_name, index_file, modifier):
     return [ent for ent in sorted(set(entities))]
 
 
+
+def make_date(doc):
+    if nb_str := doc.any_xpath("//tei:bibl/tei:date/@notBefore"):
+        nb_str = date_str = nb_str[0]
+        na_str = doc.any_xpath("//tei:bibl/tei:date/@notAfter")[0]
+    elif nb_str := doc.any_xpath("//tei:bibl/tei:date/@when"):
+        nb_str = na_str = date_str = nb_str[0]
+    elif date_str := doc.any_xpath("//tei:bibl/tei:date/text()"):
+        date_str = date_str[0].split("--")[0]
+        if len(date_str) > 3:
+            na_str = nb = date_str
+        else:
+            date_str = na_str = nb_str = "1970-12-31"
+    else:
+        nb_str = "1300-01-01"
+        na_str = "1799-12-31"
+        date_str = "2024-04-02"
+    nb_tst = int(datetime.strptime(nb_str, "%Y-%m-%d").timestamp())
+    na_tst = int(datetime.strptime(na_str, "%Y-%m-%d").timestamp())
+    return date_str, nb_tst, na_tst
+
+
+def make_type(doc):
+    provenance = doc.any_xpath("//tei:provenance/placeName/text()")
+    if liturgy := doc.any_xpath("//tei:classDecl/tei:taxonomy[@xml:id='liturgies']/tei:catDesc/text()"):
+        liturgy = liturgy[0]
+    else:
+        liturgy = ""
+    genre = doc.any_xpath("//tei:classDecl/tei:taxonomy[@xml:id='booktypes']/tei:catDesc/text()")
+    return liturgy, genre, provenance
+
 contents = nocontents = []
 # %%
 # msContents class="#ofm #graduale #sequentiar"
@@ -80,6 +114,16 @@ for xml_filepath in tqdm(files, total=len(files)):
     doc = TeiReader(xml=xml_filepath)
     facs = doc.any_xpath(".//tei:body/tei:div/tei:pb/@facs")
     pages = 0
+    xml_file = os.path.basename(xml_filepath)
+    html_file = xml_file.replace(".xml", ".html")
+    id = os.path.splitext(xml_file)[0]
+    r_title = " ".join(
+            " ".join(
+                doc.any_xpath('.//tei:titleStmt/tei:title[@type="main"]/text()')
+            ).split()
+        )
+    date_str, nb_tst, na_tst = make_date(doc)
+    liturgy, genre, provenance = make_type(doc)
     for v in facs:
         # p_group = f".//tei:body/tei:div/tei:p[preceding-sibling::tei:pb[1]/@facs='{v}']|"\
         #    f".//tei:body/tei:div/tei:lg[preceding-sibling::tei:pb[1]/@facs='{v}']"
@@ -93,39 +137,14 @@ for xml_filepath in tqdm(files, total=len(files)):
             "project": "ofm_graz",
         }
         record = {}
-        xml_file = os.path.basename(xml_filepath)
-        html_file = xml_file.replace(".xml", ".html")
-        id = os.path.splitext(xml_file)[0]
         record["id"] = id
         cfts_record["id"] = id
         cfts_record["resolver"] = f"/{html_file}"
         record["rec_id"] = os.path.split(xml_file)[-1]
         cfts_record["rec_id"] = record["rec_id"]
-        r_title = " ".join(
-            " ".join(
-                doc.any_xpath('.//tei:titleStmt/tei:title[@type="main"]/text()')
-            ).split()
-        )
         record["title"] = f"{r_title}"  # + " Page {str(pages)}"
 
         cfts_record["title"] = record["title"]
-        if nb_str := doc.any_xpath("//tei:bibl/tei:date/@notBefore"):
-            nb_str = date_str = nb_str[0]
-            na_str = doc.any_xpath("//tei:bibl/tei:date/@notAfter")[0]
-        elif nb_str := doc.any_xpath("//tei:bibl/tei:date/@when"):
-            nb_str = na_str = date_str = nb_str[0]
-        elif date_str := doc.any_xpath("//tei:bibl/tei:date/text()"):
-            date_str = date_str[0].split("--")[0]
-            if len(date_str) > 3:
-                na_str = nb = date_str
-            else:
-                date_str = na_str = nb_str = "1970-12-31"
-        else:
-            nb_str = "1300-01-01"
-            na_str = "1799-12-31"
-            date_str = "2024-04-02"
-        nb_tst = int(datetime.strptime(nb_str, "%Y-%m-%d").timestamp())
-        na_tst = int(datetime.strptime(na_str, "%Y-%m-%d").timestamp())
         try:
             record["year"] = cfts_record["year"] = date_str
             record["notbefore"] = cfts_record["notbefore"] = nb_tst
@@ -146,22 +165,14 @@ for xml_filepath in tqdm(files, total=len(files)):
             cfts_record["doc-type"] = 'Unbekannt'
         if paragraph := doc.any_xpath(".//tei:body/tei:div/tei:ab"):
             paragraph = paragraph[0]
-            for t in paragraph:
-                pass
-            if True:
-                full_text = extract_fulltext(paragraph)
-                record["full_text"] = full_text
-                cfts_record["full_text"] = full_text
-                anchor_link = t.xpath("./@facs")[0]
-                record["anchor_link"] = anchor_link
-                cfts_record["anchor_link"] = anchor_link
-                records.append(record)
-                cfts_records.append(cfts_record)
-                if full_text:
-                    print(full_text)
-            # # print(type(body))
-            # record["full_text"] = ' '.join([extract_fulltext(p) for p in doc.any_xpath(".//tei:p")])
-# print(make_index)
+            full_text = extract_fulltext(paragraph)
+            record["full_text"] = full_text
+            cfts_record["full_text"] = full_text
+            anchor_link = t.xpath("./@facs")[0]
+            record["anchor_link"] = anchor_link
+            cfts_record["anchor_link"] = anchor_link
+            records.append(record)
+            cfts_records.append(cfts_record)
 make_index = client.collections["ofm_graz"].documents.import_(records)
 # print(make_index)
 print("done with indexing ofm_graz")

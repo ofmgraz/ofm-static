674524edb7376982af6a9f744344948afb8aa813
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-04-24 10:47:54 +0200

    index made

--- make_ts_index.py
+++ make_ts_index.py
@@ -24,7 +24,7 @@ current_schema = {
         {"name": "id", "type": "string"},
         {"name": "rec_id", "type": "string"},
         {"name": "title", "type": "string"},
-        {"name": "anchor_link", "type": "string"},
+        # {"name": "anchor_link", "type": "string"},
         {"name": "full_text", "type": "string", "optional": True},
         {"name": "notbefore", "type": "int64", "facet": True, "optional": True},
         {"name": "notafter", "type": "int64", "facet": True, "optional": True},
@@ -33,46 +33,17 @@ current_schema = {
         {"name": "doc_type", "type": "string[]", "facet": True, "optional": True},
         {"name": "liturgy", "type": "string", "facet": True, "optional": True},
         {"name": "provenance", "type": "string[]", "facet": True, "optional": True},
-        {"name": "persons", "type": "string[]", "facet": True, "optional": True},
+        {"name": "printer", "type": "string", "facet": True, "optional": True},
 
     ],
 }
 
-
-# %%
 try:
     client.collections["ofm_graz"].delete()
 except ObjectNotFound:
     pass
 
-# %%
-# client.collections["STB"].delete()
-
-# %%
 client.collections.create(current_schema)
-
-
-# ent_type="person", ent_node="person", ent_name="persName", index_file=persons_idx, modifier='@type="label"'
-# %% person ??person  persName
-def get_entities(ent_type, ent_node, ent_name, index_file, modifier):
-    entities = []
-    e_path = f'.//tei:rs[@type="{ent_type}"]/@ref'
-    for p in body:
-        ent = p.xpath(e_path, namespaces={"tei": "http://www.tei-c.org/ns/1.0"})
-        ref = [ref.replace("#", "") for e in ent if len(ent) > 0 for ref in e.split()]
-        for r in ref:
-            p_path = f'.//tei:{ent_node}[@xml:id="{r}"]//tei:{ent_name}[{modifier}][1]'
-            en = index_file.any_xpath(p_path)
-            if en:
-                entity = " ".join(" ".join(en[0].xpath(".//text()")).split())
-                if len(entity) != 0:
-                    entities.append(entity)
-                else:
-                    with open("log-entities.txt", "a") as f:
-                        f.write(f"{r} in {record['id']}\n")
-    return [ent for ent in sorted(set(entities))]
-
-
 
 def make_date(doc):
     if nb_str := doc.any_xpath("//tei:bibl/tei:date/@notBefore"):
@@ -96,17 +67,21 @@ def make_date(doc):
 
 
 def make_type(doc):
-    provenance = doc.any_xpath("//tei:provenance/placeName/text()")
-    if liturgy := doc.any_xpath("//tei:classDecl/tei:taxonomy[@xml:id='liturgies']/tei:catDesc/text()"):
+    provenance = doc.any_xpath("//tei:provenance/tei:placeName/text()")
+    if liturgy := doc.any_xpath(".//tei:taxonomy[@xml:id='liturgies']/tei:category/tei:catDesc/text()"):
         liturgy = liturgy[0]
     else:
         liturgy = ""
-    genre = doc.any_xpath("//tei:classDecl/tei:taxonomy[@xml:id='booktypes']/tei:catDesc/text()")
-    if form := doc.any_xpath("//tei:objectDesc/@form"):
+    genre = doc.any_xpath(".//tei:taxonomy[@xml:id='booktypes']/tei:category/tei:catDesc/text()")
+    if form := doc.any_xpath(".//tei:objectDesc/@form"):
         form = form[0]
     else:
         form = ""
-    return liturgy, genre, provenance, form
+    if printer := doc.any_xpath(".//tei:standOff/tei:listPerson/tei:person/tei:persName/*/text()"):
+        printer = " ".join(printer)
+    else:
+        printer = ""
+    return liturgy, genre, provenance, form, printer
 
 contents = nocontents = []
 # %%
@@ -127,7 +102,7 @@ for xml_filepath in tqdm(files, total=len(files)):
             ).split()
         )
     date_str, nb_tst, na_tst = make_date(doc)
-    liturgy, doc_type, provenance, form = make_type(doc)
+    liturgy, doc_type, provenance, form, printer = make_type(doc)
     for v in facs:
         # p_group = f".//tei:body/tei:div/tei:p[preceding-sibling::tei:pb[1]/@facs='{v}']|"\
         #    f".//tei:body/tei:div/tei:lg[preceding-sibling::tei:pb[1]/@facs='{v}']"
@@ -141,38 +116,30 @@ for xml_filepath in tqdm(files, total=len(files)):
             "project": "ofm_graz",
         }
         record = {}
-        record["id"] = id
-        cfts_record["id"] = id
-        cfts_record["resolver"] = f"/{html_file}"
-        record["rec_id"] = os.path.split(xml_file)[-1]
-        cfts_record["rec_id"] = record["rec_id"]
-        record["title"] = f"{r_title}"  # + " Page {str(pages)}"
-        cfts_record["title"] = record["title"]
-        try:
-            record["year"] = cfts_record["year"] = date_str
-            record["notbefore"] = cfts_record["notbefore"] = nb_tst
-            record["notafter"] = cfts_record["notafter"] = na_tst
-        except ValueError:
-            pass
-        record["provenance"] = provenance
-        cfts_record["provenance"] = provenance
-        record["doc_type"] = doc_type
-        cfts_record["doc_type"] = doc_type
-        record["liturgy"] = liturgy
-        cfts_record["liturgy"] = liturgy
-
-        record["form"] = form
-        cfts_record["form"] = form
         if paragraph := doc.any_xpath(".//tei:body/tei:div/tei:ab"):
             paragraph = paragraph[0]
             full_text = extract_fulltext(paragraph)
-            record["full_text"] = full_text
-            cfts_record["full_text"] = full_text
-            anchor_link = t.xpath("./@facs")[0]
-            record["anchor_link"] = anchor_link
-            cfts_record["anchor_link"] = anchor_link
-            records.append(record)
-            cfts_records.append(cfts_record)
+
+        for r in [cfts_record, record]:
+            r["id"] = id
+            r["resolver"] = f"/{html_file}"
+            r["rec_id"] = os.path.split(xml_file)[-1]
+            r["title"] = f"{r_title}"  # + " Page {str(pages)}"
+            try:
+                r["year"] = date_str
+                r["notbefore"] = nb_tst
+                r["notafter"] = na_tst
+            except ValueError:
+                pass
+            r["provenance"] = provenance
+            r["doc_type"] = doc_type
+            r["liturgy"] = liturgy
+            r["printer"] = printer
+            r["form"] = form
+            if paragraph:
+                r["full_text"] = full_text
+        records.append(record)
+        cfts_records.append(cfts_record)
 make_index = client.collections["ofm_graz"].documents.import_(records)
 # print(make_index)
 print("done with indexing ofm_graz")

acf4867d3836e703e8554dfdce09b0bfe57452cf
fsanzl <fsanzl@users.noreply.github.com>
2024-05-15 16:54:31 +0000

    deploy: d6e66d2db57cf4fd62f6c8bc156f479fc8eab60c

--- js/osd_nofrills.js
+++ js/osd_nofrills.js
@@ -5,12 +5,32 @@ for(var index=0;index < pb_elements.length;index++){
     tileSources.push({type: "image", url: pb_elements[index].getAttribute("source")})
 } ;
 
+
+function isMobile(x) {
+    if (x.matches) { // If media query matches
+      var position = "horizontal";
+    } else {
+      var position = "vertical"
+    }
+    return position
+  }
+  
+  // Create a MediaQueryList object
+  var x = window.matchMedia("(max-width: 700px)")
+
+  position = isMobile(x)
+  x.addEventListener("change", function() {
+    isMobile(x);
+  }); 
   
   var viewer = OpenSeadragon({
     id: "seadragon-viewer",
     prefixUrl: "//openseadragon.github.io/openseadragon/images/",
+    panVertical : false,
+    panHorizontal: true,
     sequenceMode: true,
     showReferenceStrip: true,
-    tileSources: tileSources
+    tileSources: tileSources,
+    referenceStripScroll: position
   });
   

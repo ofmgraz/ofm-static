533557c83bc03b40daf294037f736afa625c32d1
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2025-01-31 09:45:30 +0100

    more accurate

--- html/js/ts_search.js
+++ html/js/ts_search.js
@@ -119,7 +119,7 @@ search.addWidgets([
     templates: {
       empty: "Keine Resultate für <q>{{ query }}</q>",
       item: `
-              <h5><a href="{{base_url}}{{resolver}}{{anchor_link}}">{{#helpers.snippet}}{ "attribute": "title", "highlightedTagName": "mark" }{{/helpers.snippet}}</a></h5>
+              <h5><a href="{{base_url}}{{resolver}}{{anchor_link}}" target="_blank">{{#helpers.snippet}}{ "attribute": "title", "highlightedTagName": "mark" }{{/helpers.snippet}}</a></h5>
               <p style="overflow:hidden;max-height:210px;">{{#helpers.snippet}}{ "attribute": "full_text", "highlightedTagName": "mark" }{{/helpers.snippet}}</p>
               <!-- <h5><span class="badge badge-primary">{{ project }}</span></h5> -->
           `,

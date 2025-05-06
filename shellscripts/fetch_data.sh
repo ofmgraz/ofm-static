0d20dc36648a30c9939b873bd1fed7b6f82503c3
Fernando Sanz-Lázaro <54422069+fsanzl@users.noreply.github.com>
2025-05-06 10:53:58 +0200

    Update fetch_data.sh

--- shellscripts/fetch_data.sh
+++ shellscripts/fetch_data.sh
@@ -4,9 +4,11 @@ echo "fetching transkriptions from data_repo"
 rm -rf data/editions
 rm -rf data/indices
 rm -rf data/meta
-curl -LO https://github.com/ofmgraz/transkribus-out/archive/refs/heads/main.zip
+curl -LO https://github.com/ofmgraz/transkribus-out/archive/refs/heads/main.zip 
+# curl -LO https://github.com/ofmgraz/transkribus-out/archive/refs/heads/dev.zip 
 unzip main
 mv ./transkribus-out-main/data/editions ./transkribus-out-main/data/indices ./data
+# mv ./transkribus-out-main/data/editions ./transkribus-out-dev/data/indices ./data
 echo `ls data`
 
 curl -LO https://github.com/ofmgraz/ofm-para-text/archive/refs/heads/main.zip
@@ -14,6 +16,8 @@ unzip main
 mv ./ofm-para-text-main/data/meta ./data/
 
 rm main.zip
+# rm main.zip dev.zip
 rm -rf ./transkribus-out-main ./ofm-para-text-main
+# rm -rf ./transkribus-out-dev ./ofm-para-text-main 
 
 rm -f data/editions/{D,G}*
